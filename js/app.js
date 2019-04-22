80cd84ebda7fffd1ca7377c4982cfacb35ad396b
DESKTOP-6LGDSVF\German <germanllop@gmail.com>
2019-04-22 00:48:29 -0500

    transition test

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.footer-message').addClass("footer-message-off");
+    }, 3000);
+    setTimeout(function () {
+        $('.footer-message').removeClass("footer-message-off");
+    }, 4000);
+    setTimeout(function () {
+        $('.footer-message').addClass("footer-message-off");
+    }, 6000);
+  });
